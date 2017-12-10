e66a996c22828328584ca3e4b854fa1dceddf2a4
Konstantin Mazurov <streeboge@yandex.ru>
2017-12-10 20:25:47 +0700

    hidden graph on mobile

--- assets/scripts/token.js
+++ assets/scripts/token.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var oldStage = '';
 // start of auction
 var startDay = new Date('Dec 5, 2017 17:34:20').getTime();
@@ -9,7 +7,8 @@ var endDay = new Date('Oct 29, 2018 17:34:45').getTime();
 // Auction price decay function; returns price at timestamp
 function getPrice(priceStart, priceConstant, priceExponent, elapsedSeconds) {
   var decayRate = Math.floor(Math.pow(elapsedSeconds, priceExponent) / priceConstant);
-  var price = Math.floor(priceStart * (1 + elapsedSeconds) / (1 + elapsedSeconds + decayRate));
+  var price = Math.floor(priceStart * (1 + elapsedSeconds) /
+    (1 + elapsedSeconds + decayRate));
   return price;
 }
 
@@ -29,47 +28,69 @@ function updateCurrentValues(obj) {
     return;
   }
   if (obj['current-price'] !== undefined) {
-    val = obj['current-price'].toLocaleString('en', { minimumFractionDigits: 7, maximumFractionDigits: 7 });
+    val = (obj['current-price']).toLocaleString(
+      'en',
+      {minimumFractionDigits: 7, maximumFractionDigits: 7}
+    );
     $('.current-price').text(val);
   }
   if (obj['current-target'] !== undefined) {
-    var val = obj['current-target'].toLocaleString('en', { maximumFractionDigits: 0 });
+    var val = obj['current-target'].toLocaleString(
+      'en',
+      {maximumFractionDigits: 0}
+    );
     $('.current-target').text(val);
   }
   if (obj['current-total'] !== undefined) {
-    var val = obj['current-total'].toLocaleString('en', { maximumFractionDigits: 0 });
+    var val = obj['current-total'].toLocaleString(
+      'en',
+      {maximumFractionDigits: 0}
+    );
     $('.current-total').text(val);
   }
   if (obj['total-issued'] !== undefined) {
-    var val = obj['total-issued'].toLocaleString('en', { maximumFractionDigits: 0 });
+    var val = obj['total-issued'].toLocaleString(
+      'en',
+      {maximumFractionDigits: 0}
+    );
     $('.total-issued').text(val);
   }
   if (obj['amount-raised'] !== undefined) {
-    var val = obj['amount-raised'].toLocaleString('en', { maximumFractionDigits: 0 });
+    var val = obj['amount-raised'].toLocaleString(
+      'en',
+      {maximumFractionDigits: 0}
+    );
     $('.amount-raised').text(val);
   }
   if (obj['current-target'] !== undefined && obj['amount-raised'] !== undefined) {
-    var val = (obj['current-target'] - obj['amount-raised']).toLocaleString('en', { maximumFractionDigits: 0 });
+    var val = (obj['current-target'] - obj['amount-raised']).toLocaleString(
+      'en',
+      {maximumFractionDigits: 0}
+    );
     $('.current-missing').text(val);
   }
   if (obj['current-end'] !== undefined) {
-    var val = obj['current-end'].toLocaleString('en-gb', // 24h clock
-    { month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' }),
-        tz = obj['current-end'].toLocaleString('en-gb', { timeZoneName: 'short' }).split(' ').pop();;
+    var val = obj['current-end'].toLocaleString(
+      'en-gb', // 24h clock
+      {month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit'}
+    ),
+      tz = obj['current-end'].toLocaleString(
+        'en-gb',
+        {timeZoneName:'short'}).split(' ').pop();;
     $('.current-end').text(val);
     $('.current-end-tz').text(tz);
   }
 
   if (obj['auction_stage'] !== undefined) {
-    switch (obj.auction_stage) {
+    switch(obj.auction_stage) {
       case 0:
-        break;
+      break;
       case 1:
         handleStageChange('not-started');
-        break;
+      break;
       case 2:
         handleStageChange('started');
-        break;
+      break;
       case 3:
       case 4:
         handleStageChange('ended');
@@ -82,26 +103,32 @@ function updateCurrentValues(obj) {
 }
 
 function getStaticData() {
-  $.when($.getJSON('assets/scripts/static.json'), $.getJSON('assets/scripts/status.json')).done(function (staticRes, statusRes) {
-    var json = $.extend(staticRes[0], statusRes[0]['status'] || {});
-    var tokensRetained = json.totalSupply - json.tokensOffered;
-    $('.tokens-offered').text(json.tokensOffered);
-    $('.tokens-retained').text(tokensRetained);
-    $('.total-supply').text(json.totalSupply);
-    $('.starting-price').text(json.price_start / 1e18);
+  $.when($.getJSON('assets/scripts/static.json'), $.getJSON('assets/scripts/status.json'))
+    .done(function(staticRes, statusRes) {
+      var json = $.extend(staticRes[0], statusRes[0]['status'] || {});
+      var tokensRetained = json.totalSupply - json.tokensOffered;
+      $('.tokens-offered').text(json.tokensOffered);
+      $('.tokens-retained').text(tokensRetained);
+      $('.total-supply').text(json.totalSupply);
+      $('.starting-price').text(json.price_start / 1e18);
 
-    if (json.start_time) {
-      var d = new Date(json.start_time * 1000),
-          val = d.toLocaleString('en-gb', // 24h clock
-      { month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' }),
-          tz = d.toLocaleString('en-gb', { timeZoneName: 'short' }).split(' ').pop();
-      $('.auction-start').text(val);
-      $('.auction-start-tz').text(tz);
-    }
-  }).fail(function (jqxhr, textStatus, error) {
-    var err = textStatus + ', ' + error;
-    console.error('Request Failed: ', err);
-  });
+      if (json.start_time) {
+        var d = new Date(json.start_time * 1000),
+          val = d.toLocaleString(
+          'en-gb', // 24h clock
+          {month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit'}
+        ),
+          tz = d.toLocaleString(
+            'en-gb',
+            {timeZoneName:'short'}).split(' ').pop();
+        $('.auction-start').text(val);
+        $('.auction-start-tz').text(tz);
+      }
+    })
+    .fail(function(jqxhr, textStatus, error) {
+      var err = textStatus + ', ' + error;
+      console.error('Request Failed: ', err);
+    });
 }
 
 getStaticData();
@@ -174,9 +201,7 @@ function updateAuctionAddress(addr) {
   // $('.address').attr('value', addr);
 }
 
-var countdown = setInterval(function () {
-  ticker();
-}, 1000);
+var countdown = setInterval(function() { ticker(); }, 1000);
 
 function ticker() {
   var now = Date.now();
@@ -191,9 +216,9 @@ function ticker() {
   }
 
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
-  var minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
-  var seconds = Math.floor(distance % (1000 * 60) / 1000);
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   if (days < 10) days = '0' + days;
   if (hours < 10) hours = '0' + hours;
@@ -217,10 +242,10 @@ function ticker() {
 }
 
 //button to show or hide the eth address
-$(document).ready(function () {
+$(document).ready(function() {
   // tooltips
   $('[data-toggle="tooltip"]').tooltip({
-    placement: function placement(ctx, src) {
+    placement: function(ctx, src) {
       if ($(window).width() < 1200) {
         return 'bottom';
       } else {
@@ -229,7 +254,7 @@ $(document).ready(function () {
     }
   });
   $('#auctionrunning').hide();
-  $('#agreetac').click(function () {
+  $('#agreetac').click(function() {
     $('#auctionrunning').show();
     $('#timer-container').hide();
     $('#contribute-btn').hide();
@@ -241,35 +266,35 @@ $(document).ready(function () {
   $('.se-pre-con').delay(2000).fadeOut(1000);
 });
 
-$('#disagreetac').click(function () {
+$('#disagreetac').click(function() {
   $.ajax({
-    success: function success(result) {
+    success : function(result) {
       $('#auctionrunning').hide();
     }
   });
 });
 
-$('#print').click(function () {
-  $('#terms1').print();
+$('#print').click(function() {
+  $('#terms1').print()
 });
 
 ticker();
 
-$('#auctionaddress').click(function ($event) {
+$('#auctionaddress').click(function($event){
   $event.target.select();
 });
 
-$('#copyButton').click(function () {
+$('#copyButton').click(function(){
   $('#auctionaddress').select();
   document.execCommand('copy');
 });
 
-$('#price').bind('input change keyup', function ($event) {
+$('#price').bind('input change keyup', function($event) {
   var val = +$event.target.value;
   $('#offeredvalue').val(Math.floor(val * 50e6));
 });
 
-$('#offeredvalue').bind('input change keyup', function ($event) {
+$('#offeredvalue').bind('input change keyup', function($event) {
   var val = +$event.target.value;
   $('#price').val((val / 50e6).toFixed(7));
 });
@@ -277,29 +302,34 @@ $('#offeredvalue').bind('input change keyup', function ($event) {
 function checkAlertsForm() {
   var priceMax = getCurrentPriceAt(new Date()),
       priceMin = getCurrentPriceAt(maxTime),
-      valueMax = Math.floor(priceMax * totalIssued);
-  valueMin = Math.floor(priceMin * totalIssued);
+      valueMax = Math.floor(priceMax * totalIssued)
+      valueMin = Math.floor(priceMin * totalIssued);
   $('#alerts-form #price').attr('max', priceMax.toFixed(7));
   $('#alerts-form #price').attr('min', priceMin.toFixed(7));
   $('#alerts-form #offeredvalue').attr('max', valueMax);
   $('#alerts-form #offeredvalue').attr('min', valueMin);
 
   var price = +$('#alerts-form #price').val();
-  var valid = $('#alerts-form #email').val().match(/@/) && price && grecaptcha.getResponse() && $('#noLiability').is(':checked') && priceMin <= price && price <= priceMax;
-  $('#alerts-confirm').prop('disabled', !valid);
+  var valid = $('#alerts-form #email').val().match(/@/) &&
+    price &&
+    grecaptcha.getResponse() &&
+    $('#noLiability').is(':checked') &&
+    priceMin <= price && price <= priceMax;
+  $('#alerts-confirm')
+      .prop('disabled', !valid);
   return valid;
 }
 $('#alerts-form input').bind('change keyup input', checkAlertsForm);
-$('#alerts-form').on('submit', function (e) {
+$('#alerts-form').on('submit', function(e) {
   var valid = checkAlertsForm();
   if (!valid || $('#alerts-confirm').prop('disabled')) {
     e.preventDefault();
   }
 });
 
-$('#alerts-confirm').click(function (e) {
-  checkAlertsForm();
-  e.preventDefault();
+$('#alerts-confirm').click(function(e) {
+  checkAlertsForm()
+  e.preventDefault()
   if (!$('#alerts-confirm').prop('disabled')) {
     $.post('https://pricealert.token.raiden.network/set_alert', {
       email: $('#email').val(),
@@ -307,17 +337,17 @@ $('#alerts-confirm').click(function (e) {
       price: $('#price').val(),
       noLiability: true,
       'g-recaptcha-response': grecaptcha.getResponse()
-    }).done(function () {
+    }).done(function() {
       $('#price-alert-error').hide();
       $('#price-alert-success').show();
-    }).fail(function () {
+    }).fail(function() {
       $('#price-alert-success').hide();
       $('#price-alert-error').show();
-    }).always(function () {
+    }).always(function() {
       $('#price-alert-info').hide();
       $('#alerts-form').hide();
       $('#alerts-confirm').hide();
-    });
+    })
   }
 });
 
@@ -347,4 +377,3 @@ function setAlertsValue() {
   $('#offeredvalue').val(value);
   $('#alerts-btn').click();
 }
-//# sourceMappingURL=token.js.map
